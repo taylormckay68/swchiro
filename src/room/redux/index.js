8378e3726c305df97486639b7e39265980bca5f5
Taylor McKay <tmckay@overstock.com>
2018-10-31 16:30:07 -0600

    removed /room

--- src/room/redux/index.js
+++
@@ -1,7 +0,0 @@
-import rootReducer from './rootReducer'
-import { fetchMediaItem, fetchSimilarRooms, fetchProducts } from './actions'
-
-export { fetchMediaItem, fetchSimilarRooms, fetchProducts }
-
-export default rootReducer
-
