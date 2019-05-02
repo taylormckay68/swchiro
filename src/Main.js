8674a1e1e88e631fc6b815fdb198244221232644
tmckay <tmckay@overstock.com>
2019-05-01 22:45:17 -0600

    asdf

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar';
 import router from './router';
 import {Route, Switch} from 'react-router-dom';
+import Home from './Home/Home';
 // import {} from './styled-components/Root';
 
 export default function Main() {
@@ -10,11 +11,7 @@ export default function Main() {
             <Navbar/>
             {/* {router} */}
             <Switch>
-                <Route exact path="/" render={() => {
-                    return(
-                        <div>home</div>
-                    )
-                }}/>
+                <Route exact path="/" render={() => <Home/>}/>
                 <Route path="/services" render={() => {
                     return(
                         <div>services</div>
