5e8ef39e3dc4ad456aa89b1bcdc237b802835b10
tmckay <tmckay@overstock.com>
2019-05-01 23:11:25 -0600

    test

--- build/home/Test.js
+++ build/home/Test.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -16,10 +18,33 @@ var _Hero2 = _interopRequireDefault(_Hero);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = function (Home) {
-    return _react2.default.createElement(
-        _Home.HomeWrapper,
-        null,
-        _react2.default.createElement(_Hero2.default, null)
-    );
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Test = function (_Component) {
+    _inherits(Test, _Component);
+
+    function Test() {
+        _classCallCheck(this, Test);
+
+        return _possibleConstructorReturn(this, (Test.__proto__ || Object.getPrototypeOf(Test)).apply(this, arguments));
+    }
+
+    _createClass(Test, [{
+        key: 'render',
+        value: function render() {
+            return _react2.default.createElement(
+                _Home.HomeWrapper,
+                null,
+                _react2.default.createElement(_Hero2.default, null)
+            );
+        }
+    }]);
+
+    return Test;
+}(_react.Component);
+
+exports.default = Test;
--- src/home/Test.js
+++ src/home/Test.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react'
 import {HomeWrapper} from './components/styled-components/Home';
 import Hero from './components/Hero';
 
-export default Home => {
-    return(
-        <HomeWrapper>
-            <Hero/>
-        </HomeWrapper>
-    )
+export default class Test extends Component{
+    render() {
+        return(
+            <HomeWrapper>
+                <Hero/>
+            </HomeWrapper>
+        )
+    }
 }
