8520cf241369bbf91cadb57484a25d41fb8aa351
tmckay <tmckay@overstock.com>
2019-05-01 22:24:54 -0600

    removed home

--- build/router.js
+++ build/router.js
@@ -1,33 +1,27 @@
-'use strict';
+// import React from 'react';
+// import {Route, Switch} from 'react-router-dom';
+// import Home from './Home/Home';
+// import Services from './Services/Services';
+// import Contact from './Contact/Contact';
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
+// export default(
+//     <Switch>
+//         <Route exact path='/' component={Home} />
+//         <Route path='/services' component={Services} />
+//         <Route path='/contact' component={Contact} />
 
-var _react = require('react');
-
-var _react2 = _interopRequireDefault(_react);
-
-var _reactRouterDom = require('react-router-dom');
-
-var _Home = require('./Home/Home');
-
-var _Home2 = _interopRequireDefault(_Home);
-
-var _Services = require('./Services/Services');
-
-var _Services2 = _interopRequireDefault(_Services);
-
-var _Contact = require('./Contact/Contact');
-
-var _Contact2 = _interopRequireDefault(_Contact);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-exports.default = _react2.default.createElement(
-    _reactRouterDom.Switch,
-    null,
-    _react2.default.createElement(_reactRouterDom.Route, { exact: true, path: '/', component: _Home2.default }),
-    _react2.default.createElement(_reactRouterDom.Route, { path: '/services', component: _Services2.default }),
-    _react2.default.createElement(_reactRouterDom.Route, { path: '/contact', component: _Contact2.default })
-);
+//         {/* <Route path='/roster' render={() => {
+//             return (
+//                 <Roster>
+//                     <Switch>
+//                         <Route path='/roster/teama' component={TeamA} />
+//                         <Route path='/roster/teamB' component={TeamB} />                        
+//                     </Switch>
+//                 </Roster>
+//             )
+//         }}>
+//         </Route>
+//         <Route path='/schedule' component={Schedule}></Route> */}
+//     </Switch>
+// );
+"use strict";
--- src/router.js
+++ src/router.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import {Route, Switch} from 'react-router-dom';
-import Home from './Home/Home';
-import Services from './Services/Services';
-import Contact from './Contact/Contact';
+// import React from 'react';
+// import {Route, Switch} from 'react-router-dom';
+// import Home from './Home/Home';
+// import Services from './Services/Services';
+// import Contact from './Contact/Contact';
 
-export default(
-    <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/services' component={Services} />
-        <Route path='/contact' component={Contact} />
+// export default(
+//     <Switch>
+//         <Route exact path='/' component={Home} />
+//         <Route path='/services' component={Services} />
+//         <Route path='/contact' component={Contact} />
 
-        {/* <Route path='/roster' render={() => {
-            return (
-                <Roster>
-                    <Switch>
-                        <Route path='/roster/teama' component={TeamA} />
-                        <Route path='/roster/teamB' component={TeamB} />                        
-                    </Switch>
-                </Roster>
-            )
-        }}>
-        </Route>
-        <Route path='/schedule' component={Schedule}></Route> */}
-    </Switch>
-);
+//         {/* <Route path='/roster' render={() => {
+//             return (
+//                 <Roster>
+//                     <Switch>
+//                         <Route path='/roster/teama' component={TeamA} />
+//                         <Route path='/roster/teamB' component={TeamB} />                        
+//                     </Switch>
+//                 </Roster>
+//             )
+//         }}>
+//         </Route>
+//         <Route path='/schedule' component={Schedule}></Route> */}
+//     </Switch>
+// );
